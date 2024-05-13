23d143aff80bc5efc8a6e0c36173b98f994bc578
CRAMBES07 <juju.crambes@gmail.com>
2024-05-13 13:58:49 +0200

    ready project for deployment on pypi

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+Paradi
+Python Abstract Restful Api Dialoger Interface
+This is an abstract class which purpose is to interface any Rest API using python's standard functionalities.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Julien Crambes'
