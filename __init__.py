8d6f4a4ae0e66a1acb1a1487afd88ea18697179e
Julien Crambes <juju.crambes@gmail.com>
2024-05-13 16:45:51 +0200

    Update __init__.py
    adding dynamic versionning

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ Python Abstract Restful Api Dialoger Interface
 This is an abstract class which purpose is to interface any Rest API using python's standard functionalities.
 """
 
-__version__ = "0.1.2"
+__version__ = "{{ VERSION_PLACEHOLDER }}"
 __author__ = 'Julien Crambes'
