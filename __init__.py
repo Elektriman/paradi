283f4e09c4d377dbe0e06aa28fe415efc7c8e09f
Julien Crambes <juju.crambes@gmail.com>
2024-05-14 08:31:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ Python Abstract Restful Api Dialoger Interface
 This is an abstract class which purpose is to interface any Rest API using python's standard functionalities.
 """
 
-__version__ = "{{ VERSION_PLACEHOLDER }}"
+__version__ = "{{VERSION_PLACEHOLDER}}"
 __author__ = 'Julien Crambes'
