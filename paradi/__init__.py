bc93b4ae22f68b44794bcfa95a1feffc46927dc1
CRAMBES07 <juju.crambes@gmail.com>
2024-05-14 10:35:43 +0200

    update version

--- paradi/__init__.py
+++ paradi/__init__.py
@@ -4,7 +4,7 @@ Python Abstract Restful Api Dialoger Interface
 This is an abstract class which purpose is to interface any Rest API using python's standard functionalities.
 """
 
-__version__ = "0.1.3.post1"
+__version__ = "0.1.3.post2"
 __author__ = 'Julien Crambes'
 
 from .paradi import Paradi
